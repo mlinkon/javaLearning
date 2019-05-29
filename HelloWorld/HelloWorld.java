d90e26e990e0d02142daecdcd10edb408a277b37
linkon <linkon099@gmail.com>
2019-05-29 11:25:44 +0530

    HelloWorld program

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld{
+    public static void main (String args[]){
+        System.out.println("Hello World");
+    }
+
+
+}
+
+/*
+command to run the program 
+@@compile : javac Helloworld.java
+@@run     : java HelloWorld
+
+*/
