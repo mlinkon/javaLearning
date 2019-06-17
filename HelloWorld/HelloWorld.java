b110ce61a1f0f267ec3c48b4e6b4206a984596f2
linkon <linkon099@gmail.com>
2019-06-17 13:51:46 +0530

    Update in HelloWorld class , to showcase the start of execution of class and to call another method after the start.

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,6 +1,12 @@
 public class HelloWorld{
     public static void main (String args[]){
         System.out.println("Hello World");
+        main(Integer.parseInt(args[0])); //calling other main method having different signature as 
+        //execution starts from the method having signature public static void main (String args[])
+    }
+    public static void main(int a){
+        System.out.println("value of a is "+ a);
+
     }
 
 
